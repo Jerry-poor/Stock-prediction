89291a11afcad49156596e07c66a452179e4b688
Jerry-poor <jerrylikespython@gmail.com>
2025-03-21 13:18:14 +0800

    inital version

---
+++ data.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+df = pd.read_csv("./dataset/QQQ.csv")
+df["Date"] = pd.to_datetime(df["Date"], format="%m/%d/%Y")
+
+train_df = df[(df["Date"].dt.year >= 2021) & (df["Date"].dt.year <= 2023)]
+new_df = train_df.copy()
+
+new_df["Rise/Fall"] = 0
+for idx in new_df.index:
+    if new_df.loc[idx, "Close/Last"] - new_df.loc[idx, "Open"]>0:
+        new_df.loc[idx, "Rise/Fall"] =1
+    
+new_df.to_csv("new_result.csv", index=False)
